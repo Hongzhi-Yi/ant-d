4202d3e0c972c09e3fdc8e3bdc9cfd9ea1fe4e3c
Gal4xy <galaxyang@icloud.com>
2020-04-21 17:01:58 +0800

    try question

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,77 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'dva'
 import {Card, Row, Col, Form, Button, Input, Checkbox} from 'antd'
-/*
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
-const onFinish = values => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed = errorInfo => {
-  console.log('Failed:', errorInfo);
-};
-interface Question{
-    title:string
-}
-const QuestionBoard: React.FC<Props> = ({ questionSetId }) => {
-    return (
-    <Card>{questionSetId}
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type="primary"  htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    {/!*<Row>
-     <Button type="primary">abc</Button>
-    </Row>*!/}
-
-    </Card>)
-}
-
-const mapStateToProps=({ router: { location: { query } }, user: { currentUser } }) => {
-    return {
-        questionSetId: query.questionSetId,
-    }
-}
-
-
-export default connect(mapStateToProps)(QuestionBoard)
-*/
 const namespace = 'puzzlecards';
 
 const mapStateToProps = (state) => {
@@ -80,7 +9,7 @@ const mapStateToProps = (state) => {
     cardList
   };
 };
-const mapDispatchToProps = (dispatch) => {
+/*const mapDispatchToProps = (dispatch) => {
   return {
     onClickAdd: (newCard) => {
       const action = {
@@ -114,6 +43,40 @@ export default class PuzzleCardsPage extends Component {
             punchline: 'here we use dva',
           })}> 添加卡片 </Button>
         </div> }
+      </div>
+    );
+  }
+}*/
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDidMount: () => {
+      dispatch({
+        type: `${namespace}/queryInitCards`,
+      });
+    },
+  };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class PuzzleCardsPage extends Component {
+  componentDidMount() {
+    this.props.onDidMount();
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.props.cardList.map(card => {
+            return (
+              <Card key={card.id}>
+                <div>Q: {card.setup}</div>
+                <div>
+                  <strong>A: {card.punchline}</strong>
+                </div>
+              </Card>
+            );
+          })
+        }
       </div>
     );
   }
